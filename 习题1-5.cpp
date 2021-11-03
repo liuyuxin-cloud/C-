73cdf005038d223fed2ee6c358e7447acb3a9f72
刘昱欣 <2590177089@qq.com>
2021-11-03 16:55:56 +0800

    9

--- 习题1-5.cpp
+++ 习题1-5.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 
 int fun(int a[],int x0,int n){
@@ -10,11 +11,11 @@ int fun(int a[],int x0,int n){
 }
 
 int main(){
-    int a[100],n,x0，sum;
+    int a[100],n, x0, sum;
     cout << "请输入多项式阶数";
     cin >> n;
     cout << "请输入依次输入系数";
-    for(i=0 ; i<n ; i++){
+    for(int i=0 ; i<n ; i++){
         cin >> a[i];
     }
     cout << "请输入未知数x0的值";
---
+++ 实验9_三元组加法.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+const int MAXSIZE = 12;
+
+typedef struct {
+    int row, col;
+    int e;
+}Triple;
+
+typedef struct {
+    Triple data[MAXSIZE+1];
+    int mu, nu, tu;
+}TSMatrix;
+
+void Translate(int T[3][4], TSMatrix &M){
+    M.mu = 3;
+    M.nu = 4;
+    M.tu = 0;
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 4; j++){
+            if(T[i][j] != 0){
+                M.data[M.tu].row = i;
+                M.data[M.tu].col = j;
+                M.data[M.tu++].e = T[i][j]; 
+            }
+        }
+    }
+}
+
+void add(TSMatrix s1, TSMatrix s2, TSMatrix &ans){
+    int m = s1.mu;
+    int n = s1.nu;
+    ans.tu = 0;
+    ans.mu = 3;
+    ans.nu = 4;
+    int t1 = 0, t2 = 0;
+    while(t1 < s1.tu && t2 < s2.tu){
+        if(s1.data[t1].row == s2.data[t2].row && s1.data[t1].col == s2.data[t2].col){
+            ans.data[ans.tu].row = s1.data[t1].row;
+            ans.data[ans.tu].col = s1.data[t1].col;
+            ans.data[ans.tu++].e = s1.data[t1++].e +s2.data[t2++].e;
+        }else if(s1.data[t1].row == s2.data[t2].row && s1.data[t1].col < s2.data[t2].col){
+            ans.data[ans.tu].row = s1.data[t1].row;
+            ans.data[ans.tu].col = s1.data[t1].col;
+            ans.data[ans.tu++].e = s1.data[t1++].e;
+        }else if(s1.data[t1].row == s2.data[t2].row && s1.data[t1].col > s2.data[t2].col){
+            ans.data[ans.tu].row = s2.data[t2].row;
+            ans.data[ans.tu].col = s2.data[t2].col;
+            ans.data[ans.tu++].e = s2.data[t2++].e;
+        }else if(s1.data[t1].row < s2.data[t2].row){
+            ans.data[ans.tu].row = s1.data[t1].row;
+            ans.data[ans.tu].col = s1.data[t1].col;
+            ans.data[ans.tu++].e = s1.data[t1++].e;
+        }else{
+            ans.data[ans.tu].row = s2.data[t2].row;
+            ans.data[ans.tu].col = s2.data[t2].col;
+            ans.data[ans.tu++].e = s2.data[t2++].e;
+        }
+    }
+}
+
+int main(){
+    int x1[3][4] = {0,0,1,0,2,0,0,1,0,1,0,0};
+    int x2[3][4] = {1,0,2,0,1,0,1,0,0,0,0,1};
+    TSMatrix s1, s2, ans;
+    Translate(x1, s1);
+    Translate(x2, s2);
+    add(s1, s2, ans);
+    for(int i = 0; i < ans.tu; i++){
+        cout << ans.data[i].e << " ";
+    }
+}
